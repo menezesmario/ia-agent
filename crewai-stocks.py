f2f57f46c689c685be1b769340d8e4b3bccee0ac
Mário Menezes <mario082@gmail.com>
2024-08-21 20:34:29 -0300

    feat: application running in the browser with streamlit. Search through a stock input in the sidebar

---
+++ crewai-stocks.py
@@ -0,0 +1,175 @@
+import json
+import os
+from datetime import datetime
+
+import yfinance as yf
+
+from crewai import Agent, Task, Crew, Process
+
+from langchain.tools import Tool
+from langchain_openai import ChatOpenAI
+from langchain_community.tools import DuckDuckGoSearchResults
+
+import streamlit as st 
+
+
+#CREATE YAHOO FINANCE TOOL
+
+def fetch_stock_price(ticket):
+    stock = yf.download(ticket, start="2023-08-08", end="2024-08-08")
+    return stock
+
+yahoo_finance_tool = Tool(
+    name = "Yahoo Finance Tool",
+    description = "Fetches stocks prices for {ticket} from the last year about a specfic company from Yahoo Finance API",
+    func= lambda ticket: fetch_stock_price(ticket)
+)
+
+
+
+#IMPORT OPENAI LLM - GPT
+os.environ['OPENAI_API_KEY'] = "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"
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+
+#CREWAI PRICE AGENT
+stockPriceAnalyst = Agent(
+    role="Senior stock price Analyst",
+    goal="find the {ticket} stock price and analyses trends",
+    backstory="""You're a hightly experienced in analyzing the price about an specific stock and make predictions about its future price.""",
+    verbose=True,
+    llm= llm,
+    max_iter= 5,
+    memory= True,
+    tools=[yahoo_finance_tool],
+    allow_delegation = False
+)
+
+
+# In[41]:
+
+
+#CREWAI PRICE TASK
+getStockPrice = Task(
+    description= "Analyze the stock {ticket} price history and reate a trend analyses of up, down or sideways",
+    expected_output= """ Specify the current trend stock price - up, down or sideways.
+    eg. stock= 'AAPL, price UP'""",
+    agent= stockPriceAnalyst
+)
+
+
+
+#IMPORT A SEARCH TOOL
+
+search_tool = DuckDuckGoSearchResults(backend='news', num_results=10)
+
+
+
+#CREWAI NEWS AGENT
+newsAnalyst = Agent(
+    role="Stock News Analyst",
+    goal="""Create a short summary of the market news related to the stock {ticket} company. Specify the current trend - up, down or sideways with the news context. For each request stock asset, specify a number between 0 and 100, where 0 is extreme fear and 100 is extreme greed.""",
+    backstory="""You're highly experienced in analyzing the market trends and news and have tracked assets for more then 10 years.
+    
+    You're also master level analysts in the traditional markets and have deep understanding of human psychology.
+    
+    You understand news, theirs tittles and information, but you look at those with a health dose of skepticism. 
+    You consider alsto the source of the news.
+    """,
+    verbose=True,
+    llm= llm,
+    max_iter= 5,
+    memory= True,
+    tools=[yahoo_finance_tool],
+    allow_delegation = False
+)
+
+
+#CREWAI NEWS TASK
+getNews = Task(
+    description= f"""Take the stock and always include BTC to it(if not request).
+    Use the search tool to search each one individualy.
+
+    The current date is {datetime.now()}.
+
+    Compose the results into a helpfull report
+    """,
+    expected_output= """A summary of the overal market and one sentence summary for each request asset.
+    Include a fear/greed score for each asset based on the news. Use format:
+    <STOCK ASSET>
+    <SUMMARY BASED ON NEWS>
+    <TREND PREDICTION>
+    <FEAR/GREED SCORE>
+    """,
+    agent= newsAnalyst
+)
+
+
+
+#CREWAI SENIOR FINANCE ANALYST
+
+stockAnalystWriter = Agent(
+    role = "Senior Stock Analysts Writer",
+    goal= """Analyze the trends price and news and write an insighfull compelling and informative 3 paragraph long newsletter based on the stock report and price trend. """,
+    backstory = """You're widely accepted as the best stock analyst in the market. You understand complex concepts and create compelling stories and narratives that resonate with wider audiences.
+    
+    You understand macro factors and combine multiple theories - eg. cycle theory and fundamental analysis. You're able to hold multiple opinions when analyzing anything.
+    """, 
+    verbose=True,
+    llm=llm,
+    max_iter = 5,
+    memory=True,
+    allow_delegation = True,
+)
+
+
+#CREWAI SENIOR FINANCE ANALYST TASK
+writeAnalyses = Task(
+    description= """ Use the stock price trend and the sotck news report to create an analyses and write the newsletter about the {ticket} company that is brief and hightlights the most important points.
+    Focus on the stock price trend, news and fear/greed score. What are the near future considerations?
+    Include the previous analyses of stock trend and news summary.
+""",
+    expected_output= """An eloquent 3 parapgraphs newsletter formated as markdown in an easy readable manner. It should contain:
+
+    - 3 bullets executive summary
+    - Introduction - set the overall picture and spike up the interest
+    - main part provides the meat of the analysis including the news summary and fear/greed scores
+    - summary - key facts and concrete future trend prediction - up, down or sideways.
+         
+""",
+    agent= stockAnalystWriter,
+    context = [getStockPrice, getNews]
+)
+
+
+crew = Crew(
+    agents = [stockPriceAnalyst, newsAnalyst, stockAnalystWriter],
+    tasks = [getStockPrice, getNews, writeAnalyses],
+    verbose = 2,
+    process= Process.hierarchical ,
+    full_output=True,
+    share_crew=False,
+    manager_llm=llm,
+    max_iter=15
+)
+
+# results= crew.kickoff(inputs={'ticket': 'NVDA'})
+
+
+with st.sidebar:
+    st.header('Enter the Stock to Research')
+
+    with st.form(key='research_form'):
+        topic = st.text_input("Select the ticket")
+        submit_button = st.form_submit_button(label = "Run Research")
+
+if submit_button:
+    if not topic:
+        st.error("Please fill the ticket field")
+    else:
+        results= crew.kickoff(inputs={'ticket': topic})
+
+        st.subheader("Results of your research:")
+        st.write(results['final_output']
+)
+
